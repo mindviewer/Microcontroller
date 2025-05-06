7d8d893a65474334b0712b42c388839d102a29f4
mindviewer <122435651+mindviewer@users.noreply.github.com>
2025-05-06 19:26:42 +0800

    Add files via upload

---
+++ LABBB2/Objects/guiconf.d
@@ -0,0 +1,8 @@
+.\objects\guiconf.o: MPD_M480\M480_source\GUIConf.c
+.\objects\guiconf.o: .\MPD_M480\Include\GUI.h
+.\objects\guiconf.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\guiconf.o: .\MPD_M480\Include\GUI_ConfDefaults.h
+.\objects\guiconf.o: .\MPD_M480\Include\GUI_Type.h
+.\objects\guiconf.o: .\MPD_M480\Include\LCD.h
+.\objects\guiconf.o: .\MPD_M480\Include\Global.h
+.\objects\guiconf.o: .\MPD_M480\Include\GUI_Version.h

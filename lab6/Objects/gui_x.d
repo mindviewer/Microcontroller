f24f02fb6d86aeefb27b70e3c3b7b1ac15a7a7ea
mindviewer <122435651+mindviewer@users.noreply.github.com>
2025-05-06 19:17:11 +0800

    Add files via upload

---
+++ lab6/Objects/gui_x.d
@@ -0,0 +1,8 @@
+.\objects\gui_x.o: MPD_M480\M480_source\GUI_X.c
+.\objects\gui_x.o: .\MPD_M480\Include\GUI.h
+.\objects\gui_x.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\objects\gui_x.o: .\MPD_M480\Include\GUI_ConfDefaults.h
+.\objects\gui_x.o: .\MPD_M480\Include\GUI_Type.h
+.\objects\gui_x.o: .\MPD_M480\Include\LCD.h
+.\objects\gui_x.o: .\MPD_M480\Include\Global.h
+.\objects\gui_x.o: .\MPD_M480\Include\GUI_Version.h
---
+++ lab6/Objects/startup_m480.d
@@ -0,0 +1,1 @@
+.\objects\startup_m480.o: RTE\Device\M487SIDAE\startup_M480.s
